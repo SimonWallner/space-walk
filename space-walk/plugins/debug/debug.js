b9d22df277481254ec5429837fa7c2252d6ea59e
Simon Wallner <me@simonwallner.at>
2014-08-07 16:51:39 +0200

    added timers to the debugger view

--- space-walk/plugins/debug/debug.js
+++ space-walk/plugins/debug/debug.js
@@ -1,5 +1,48 @@
 var libsw = new LibSpaceWalk();
 
+var Timer = function() {
+	var msSpan = document.getElementById('data-ms');
+	var fpsSpan = document.getElementById('data-fps');
+	var that = this;
+
+	this.lastT = performance.now();
+	this.deltaT = 0;
+
+	this.tick = function() {
+		var currentT = performance.now();
+		this.deltaT = currentT - this.lastT;
+		this.lastT = currentT;
+	};
+	
+	this.print = function() {
+		msSpan.innerHTML = that.deltaT.toFixed(2);
+		fpsSpan.innerHTML = (1 / that.deltaT * 1000).toFixed(2);
+	};
+};
+
+timer1 = new Timer();
+timer2 = new Timer();
+timer3 = new Timer();
+
+window.onload = function() {
+	window.requestAnimationFrame(function recurse() {
+		timer1.tick();
+		$('#anim-frame').text(timer1.deltaT.toFixed(2));
+
+		window.requestAnimationFrame(recurse);
+	})
+
+	window.setInterval(function() {
+		timer2.tick();
+		$('#interval').text(timer2.deltaT.toFixed(2));	
+	}, 0)
+}
+
+
 libsw.onMessage = function(data) {
-	$('#dump').append(new Date().toString() + ': message got: ' + JSON.stringify(data) + '<br>');	
+	// $('#dump').append(new Date().toString() + ': message got: ' + JSON.stringify(data) + '<br>');	
+	// $('#dump').text(new Date().toString() + ': message got: ' + JSON.stringify(data));
+
+	timer3.tick();
+	$('#message').text(timer3.deltaT.toFixed(2));
 }	
