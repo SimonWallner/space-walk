97fad55ac0edafff75551a702b9f8b9521d3414c
Simon Wallner <me@simonwallner.at>
2015-05-04 11:15:38 +0200

    added spinning cube, to test if it runs in background. (it does, but does not poll inputs)

---
+++ tools/input-logger-unity/input logger/Assets/spin.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spin : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		gameObject.transform.rotation *= Quaternion.Euler (0, 90.0f * Time.deltaTime, 0);
+	}
+}
